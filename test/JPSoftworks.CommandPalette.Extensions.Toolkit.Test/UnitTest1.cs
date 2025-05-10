e1a746c19a03c79d8cc2c92920bb6a93e88ed045
Jiří Polášek <me@jiripolasek.com>
2025-05-10 20:50:29 +0200

    Add empty unit test project and move shared MSBuild properties to Directory.Build.props

---
+++ test/JPSoftworks.CommandPalette.Extensions.Toolkit.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------
+//
+// Copyright (c) Jiří Polášek. All rights reserved.
+//
+// ------------------------------------------------------------
+
+namespace JPSoftworks.CommandPalette.Extensions.Toolkit.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
